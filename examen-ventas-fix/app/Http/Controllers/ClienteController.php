b60d3331a796987c09241cf507afd8a81874f4c9
Laura Figueroa <laufigueroasalas@gmail.com>
2024-09-15 00:12:08 -0300

    dev: "added CRUD functions to ProductoController, ClienteController, and UserController"

--- examen-ventas-fix/app/Http/Controllers/ClienteController.php
+++ examen-ventas-fix/app/Http/Controllers/ClienteController.php
@@ -2,9 +2,140 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cliente;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class ClienteController extends Controller
 {
-    //
+
+    public function getById($_id)
+    { //Validar sesión activa
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        } //Si no hay ID en el parámetro, me trae todos los clientes
+        if ($_id === null) {
+            $datos = User::all();
+            $datos->each(function ($item) {
+                if ($item->imagen) {
+                    $item->imagen = base64_encode($item->imagen);
+                }
+            });
+        } else {
+            $datos = User::findOrFail($_id);
+            if ($datos->imagen) {
+                $datos->imagen = base64_encode($datos->imagen);
+            }
+        }
+        return response()->json([
+            'data' => $datos
+        ]);
+    }
+
+    public function delete($_id)
+    {
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        }
+        $registro = Cliente::findOrFail($_id);
+        try {
+            $registro->delete();
+            return redirect()->route('mantenedores.index')->with('success', "[id: $registro->id] [Registro: $registro->nombre] eliminado con éxito.");
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $this->getTextToast($this->properties['title']['singular'], 'disable', 'error', $registro->nombre, null) . $e->getMessage());
+        }
+    }
+
+    public function create(Request $_request)
+    {
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        }
+        // Validar la solicitud. USO DE UNIQUE: unique:tabla,campo
+        $_request->validate([
+            'usuario_nombre' => 'required|string',
+            'usuario_rut' => 'required|string|unique:users,email',
+            'usuario_email' => 'required|string',
+            'usuario_password' => 'required|string',
+        ], $this->mensajes);
+
+        $datos = $_request->only('usuario_nombre', 'usuario_rut', 'usuario_email', 'usuario_password', 'usuario_rePassword');
+
+        if ($datos['usuario_password'] != $datos['usuario_rePassword']) {
+            return back()->withErrors(['message' => 'Las contraseñas ingresadas no son iguales.']);
+        }
+    }
+
+    public function update(Request $_request, $_id)
+    {
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        }
+
+        $_request->validate([
+            'usuario_nombre' => 'required|string',
+            'usuario_rut' => 'required|string',
+            'usuario_password' => 'required|string',
+            'usuario_imagen' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'usuario_email' => 'required',
+        ], $this->mensajes);
+
+        //busca el proyecto
+        $registro = User::findOrFail($_id);
+
+        $datos = $_request->only('_token', 'usuario_nombre', 'usuario_email', 'usuario_password', 'usuario_rePassword', 'usuario_imagen', 'usuario_rut');
+
+        $cambios = 0;
+
+        // solo si es distinto actualiza
+        if ($registro->nombre != $datos['usuario_nombre']) {
+            $registro->nombre = $datos['usuario_nombre'];
+            $cambios++;
+        }
+        if ($registro->email != $datos['usuario_email']) {
+            $registro->email = $datos['usuario_email'];
+            $cambios++;
+        }
+        if ($datos['usuario_password'] != '') {
+            if ($datos['usuario_password'] != $datos['usuario_rePassword']) {
+                return back()->withErrors(['message' => 'Las contraseñas ingresadas no son iguales.']);
+            } else {
+                $registro->password = $datos['usuario_password'];
+                $cambios++;
+            }
+        }
+        if ($registro->rol_id != $datos['usuario_rut_id']) {
+            $registro->rol_id = $datos['usuario_rut_id'];
+            $cambios++;
+        }
+
+        try {
+            if ($registro->imagen != $datos['usuario_imagen']) {
+                // Manejar la carga de la imagen
+                $image = $_request->file('usuario_imagen');
+                $imageData = file_get_contents($image);
+                $registro->imagen = $imageData;
+                $cambios += 1;
+            }
+        } catch (\Throwable $th) {
+        }
+
+        if ($cambios > 0) {
+            try {
+                $registro->save();
+                return redirect()->route('usuarios.index')->with('success', "[id: $registro->id] [Usuario: $registro->nombre] actualizado con éxito.");
+            } catch (Exception $e) {
+                return redirect()->back()->with('error', 'Error al actualizar el proyecto: ' . $e->getMessage());
+            }
+        } else {
+            return redirect()->back()->with('error', "[id: $registro->id] [Usuario: $registro->nombre] no se realizaron cambios.");
+        }
+    }
 }
--- examen-ventas-fix/app/Http/Controllers/ProductoController.php
+++ examen-ventas-fix/app/Http/Controllers/ProductoController.php
@@ -2,9 +2,119 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProductoController extends Controller
 {
-    //
+
+    private const SINGULAR_MIN = 'producto';
+    private const SINGULAR_MAY = 'Producto';
+    private const PLURAL_MIN = 'productos';
+    private const PLURAL_MAY = 'Productos';
+
+    private $properties = [
+        'title' => [
+            'genero' => 'm',
+            'name' =>  self::SINGULAR_MAY,
+            'singular' => self::SINGULAR_MAY,
+            'plural' => self::PLURAL_MAY,
+        ],
+        'view' => [
+            'index' => 'backoffice.mantenedor.' . self::SINGULAR_MIN
+        ],
+        'actions' => [
+            'new' => '/backoffice/' . self::PLURAL_MIN . '/new',
+        ],
+        'routes' => [
+            'index' => self::PLURAL_MIN . '.index'
+        ],
+        'fields' => [
+            [
+                'id' => 1,
+                'name' => 'nombre',
+                'label' => 'Nombre',
+                'control' => 'input',
+                'type' => 'text',
+                'required' => false,
+                'inEditar' => true,
+                'inNuevo' => true
+            ],
+            [
+                'id' => 2,
+                'name' => 'imagen',
+                'label' => 'Imagen',
+                'control' => 'input',
+                'type' => 'file',
+                'required' => false,
+                'inNuevo' => true
+            ],
+            [
+                'id' => 3,
+                'name' => 'descripcion',
+                'label' => 'Descripción',
+                'control' => 'textarea',
+                'type' => 'textarea',
+                'required' => false,
+                'inEditar' => true,
+                'inNuevo' => true
+            ],
+        ]
+    ];
+    public function create(Request $_request)
+    {
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        }
+        // Validar la solicitud. USO DE UNIQUE: unique:tabla,campo
+        $_request->validate([
+            'proyecto_nombre' => 'required|string|max:255',
+            'proyecto_imagen' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'proyecto_descripcion' => 'required|string|max:1000',
+        ], $this->mensajes);
+
+        // Manejar la carga de la imagen
+        $image = $_request->file('proyecto_logo');
+        $imageData = file_get_contents($image);
+        try {
+            // Insertar el registro en la base de datos
+            Producto::create([
+                'nombre' => $_request->proyecto_nombre,
+                'descripcion' => $_request->proyecto_descripcion,
+                'imagen' => $imageData,
+                'user_id_create' => $user->id,
+                'user_id_last_update' => $user->id,
+            ]);
+            return redirect()->back()->with('201', 'Proyecto creado con éxito.');
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', 'Error al crear el proyecto: ' . $e->getMessage());
+        }
+    }
+
+    public function index()
+    {
+        $user = Auth::user();
+        if ($user == NULL) {
+            return redirect()->route('usuario.login')->withErrors(['message' => 'No existe una sesión activa.']);
+        }
+        $datos = Producto::all();
+
+        //recupera datos del usuario
+        foreach ($datos as $registro) {
+            $registro->user_id_create_nombre = User::findOrFail($registro->user_id_create)->nombre;
+            $registro->user_id_last_update_nombre = User::findOrFail($registro->user_id_last_update)->nombre;
+        }
+
+        return view($this->properties['view']['index'], [
+            'user' => $user,
+            'registros' => $datos,
+            'action' => $this->properties['actions'],
+            'titulo' => $this->properties['title'],
+            'campos' => $this->properties['fields'],
+        ]);
+    }
 }
