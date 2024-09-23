0ba9679afb392c3316fc8b78ca37efef77598318
Laura Figueroa <laufigueroasalas@gmail.com>
2024-09-22 23:42:28 -0300

    dev: "Added assets to project. Modified footer, navbar, and sidebar partials to resemble template"

--- examen-ventas-fix/resources/views/backoffice/layouts/app.blade.php
+++ examen-ventas-fix/resources/views/backoffice/layouts/app.blade.php
@@ -1,53 +1,116 @@
 <!DOCTYPE html>
-<html lang="es">
-
+<html lang="en"
+class="light-style layout-navbar-fixed layout-menu-fixed layout-compact"
+dir="ltr"
+data-theme="theme-default"
+data-assets-path="../../assets/"
+data-template="vertical-menu-template-no-customizer"
+data-style="light">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>Dashboard - Analytics | Vuexy - Bootstrap Admin Template</title>
+
+  <meta name="description" content="" />
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+    rel="stylesheet" />
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="../../assets/vendor/fonts/fontawesome.css" />
+  <link rel="stylesheet" href="../../assets/vendor/fonts/tabler-icons.css" />
+  <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+
+  <!-- Core CSS -->
+
+  <link rel="stylesheet" href="../../assets/vendor/css/rtl/core.css" />
+  <link rel="stylesheet" href="../../assets/vendor/css/rtl/theme-default.css" />
+
+  <link rel="stylesheet" href="../../assets/css/demo.css" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
+
+  <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/typeahead-js/typeahead.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/apex-charts/apex-charts.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/swiper/swiper.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+  <link rel="stylesheet" href="../../assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css" />
+
+  <!-- Page CSS -->
+  <link rel="stylesheet" href="../../assets/vendor/css/pages/cards-advance.css" />
+
+  <!-- Helpers -->
+  <script src="../../assets/vendor/js/helpers.js"></script>
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="../../assets/js/config.js"></script>
 </head>
 
-<body class="hold-transition sidebar-mini sidebar-collapse layout-fixed">
+<body>
     <div class="wrapper">
-        <!-- Navbar -->
-        @include('backoffice.partials.navbar')
+        <div class="layout-page">
+            @include('backoffice.partials.sidebar')
+            <!-- Navbar -->
+            @include('backoffice.partials.navbar')
 
-        <!-- Sidebar -->
-        @include('backoffice.partials.sidebar')
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                <!-- Content -->
+                @yield('content')
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1>@yield('page-title')</h1>
-                        </div>
-                        <div class="col-sm-6 text-end">
-                            @yield('btn-add')
+                <!-- Footer -->
+                @include('backoffice.partials.footer')
+{{--                 <footer class="content-footer footer bg-footer-theme">
+                    <div class="container-xxl">
+                        <div
+                            class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+                            <div class="text-body">
+                                ©
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script>
+                                , made with ❤️ by <a href="https://pixinvent.com" target="_blank"
+                                    class="footer-link">Pixinvent</a>
+                            </div>
+                            <div class="d-none d-lg-inline-block">
+                                <a href="https://themeforest.net/licenses/standard" class="footer-link me-4"
+                                    target="_blank">License</a>
+                                <a href="https://1.envato.market/pixinvent_portfolio" target="_blank"
+                                    class="footer-link me-4">More Themes</a>
+
+                                <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/"
+                                    target="_blank" class="footer-link me-4">Documentation</a>
+
+                                <a href="https://pixinvent.ticksy.com/" target="_blank"
+                                    class="footer-link d-none d-sm-inline-block">Support</a>
+                            </div>
                         </div>
                     </div>
-                </div><!-- /.container-fluid -->
-            </section>
+                </footer> --}}
+                <!-- / Footer -->
 
-            <!-- Main content -->
-            <section class="content">
-                @yield('content')
-            </section>
-            <!-- /.content -->
-        </div>
-        <!-- /.content-wrapper -->
 
-        <!-- Footer -->
-        @include('backoffice.partials.footer')
 
-    </div>
-    <!-- ./wrapper -->
+    <!-- Footer -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 
     <!-- Custom JS -->
     @yield('scripts')
--- examen-ventas-fix/resources/views/backoffice/partials/footer.blade.php
+++ examen-ventas-fix/resources/views/backoffice/partials/footer.blade.php
@@ -1,13 +1,24 @@
-<footer class="content-footer footer bg-light">
-    <div class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 py-4">
-      <div>
-        <a href="https://demos.pixinvent.com/vuexy-html-admin-template/landing/" target="_blank" class="footer-text fw-bold">Vuexy</a> ©
-      </div>
-      <div>
-        <a href="https://themeforest.net/licenses/standard" class="footer-link me-6" target="_blank">License</a>
-        <a href="javascript:void(0)" class="footer-link me-6">Help</a>
-        <a href="javascript:void(0)" class="footer-link me-6">Contact</a>
-        <a href="javascript:void(0)" class="footer-link">Terms &amp; Conditions</a>
-      </div>
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+        <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+            <div class="text-body">
+                ©
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>2024
+                , made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
+            </div>
+            <div class="d-none d-lg-inline-block">
+                <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
+                <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More
+                    Themes</a>
+
+                <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/" target="_blank"
+                    class="footer-link me-4">Documentation</a>
+
+                <a href="https://pixinvent.ticksy.com/" target="_blank"
+                    class="footer-link d-none d-sm-inline-block">Support</a>
+            </div>
+        </div>
     </div>
-  </footer>
+</footer>
