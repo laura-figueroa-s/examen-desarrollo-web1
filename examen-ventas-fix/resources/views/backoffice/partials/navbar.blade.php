e1ee0e5e02896c861f2255641a31407930a553ae
Laura Figueroa <laufigueroasalas@gmail.com>
2024-09-28 23:40:49 -0300

    dev: "Deleted unused files on navbar when clicking profile picture"

--- examen-ventas-fix/resources/views/backoffice/partials/navbar.blade.php
+++ examen-ventas-fix/resources/views/backoffice/partials/navbar.blade.php
@@ -398,45 +398,20 @@
                         <div class="dropdown-divider my-1 mx-n2"></div>
                     </li>
                     <li>
-                        <a class="dropdown-item" href="pages-profile-user.html">
+                        <a class="dropdown-item" href="#">
                             <i class="ti ti-user me-3 ti-md"></i><span class="align-middle">My Profile</span>
                         </a>
                     </li>
                     <li>
-                        <a class="dropdown-item" href="pages-account-settings-account.html">
+                        <a class="dropdown-item" href="#">
                             <i class="ti ti-settings me-3 ti-md"></i><span class="align-middle">Settings</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="pages-account-settings-billing.html">
-                            <span class="d-flex align-items-center align-middle">
-                                <i class="flex-shrink-0 ti ti-file-dollar me-3 ti-md"></i><span
-                                    class="flex-grow-1 align-middle">Billing</span>
-                                <span
-                                    class="flex-shrink-0 badge bg-danger d-flex align-items-center justify-content-center">4</span>
-                            </span>
                         </a>
                     </li>
                     <li>
                         <div class="dropdown-divider my-1 mx-n2"></div>
                     </li>
                     <li>
-                        <a class="dropdown-item" href="pages-pricing.html">
-                            <i class="ti ti-currency-dollar me-3 ti-md"></i><span class="align-middle">Pricing</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="pages-faq.html">
-                            <i class="ti ti-question-mark me-3 ti-md"></i><span class="align-middle">FAQ</span>
-                        </a>
-                    </li>
-                    <li>
                         <div class="d-grid px-2 pt-2 pb-1">
-                            <a class="btn btn-sm btn-danger d-flex" href="auth-login-cover.html" target="_blank">
-                                <small class="align-middle">Logout</small>
-                                <i class="ti ti-logout ms-2 ti-14px"></i>
-                            </a>
-                            {{-- {{ $user }} --}}
                             <form action="{{ Route('usuario.logout') }}" method="POST">
                                 @csrf
                                 <button type="submit" class="btn btn-sm btn-danger d-flex">Cerrar Sesión</button>
